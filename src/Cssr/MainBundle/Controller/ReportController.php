e53c921a2962618bc15fe7cbde2f9a6a15445b4f
spcmky <spcmky@gmail.com>
2013-09-02 21:28:58 -0400

    entity updates

--- src/Cssr/MainBundle/Controller/ReportController.php
+++ src/Cssr/MainBundle/Controller/ReportController.php
@@ -30,5 +30,50 @@ class ReportController extends Controller
         return array();
     }
 
+    /**
+     * Get Report
+     *
+     * @Route("/view", name="report_view")
+     * @Method("GET")
+     * @Template()
+     */
+    public function viewAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $areas = $em->getRepository('CssrMainBundle:Area')->findAll();
+        $standards = $em->getRepository('CssrMainBundle:Standard')->findAll();
+
+        $scores = array();
+        for ( $i = 0; $i < 25; $i++ ) {
+            $scores[$i] = array();
+
+            $scores[$i][0] = uniqid().', '.uniqid();
+
+            $total = 0;
+            $units = 0;
+            for ( $j = 1; $j < 20; $j++ ) {
+                if ( rand(1,5) == 1 ) {
+                    $total += $scores[$i][$j] = rand(0,5);
+                    $units++;
+                } else {
+                    $scores[$i][$j] = null;
+                }
+            }
+
+            $scores[$i][23] = $total; // total units
+            $scores[$i][24] = (!$units)? 0 : round(($total/$units),1); // average
+            $scores[$i][25] = 'Gold'; // status
+        }
+
+        return array(
+            'areas' => $areas,
+            'standards' => $standards,
+            'scores' => $scores
+        );
+    }
+
+
+
 
 }
---
+++ src/Cssr/MainBundle/Form/CourseType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Cssr\MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class CourseType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('firstname')
+            ->add('middlename')
+            ->add('lastname')
+            ->add('email')
+            ->add('phone');
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Cssr\MainBundle\Entity\Course'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'cssr_mainbundle_coursetype';
+    }
+}
