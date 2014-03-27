56874d72983046370a2585eb07eea08392cef4a1
Chris Edwards <spcmky@gmail.com>
2014-03-27 09:09:04 -0400

    added checks for empty course arrays

--- src/Cssr/MainBundle/Model/Staff.php
+++ src/Cssr/MainBundle/Model/Staff.php
@@ -52,50 +52,53 @@ class Staff {
 
         // what to remove
         $removed = array_diff($currentCourses,$courses);
-        $sql  = 'SELECT C.id, C.area_id, C.user_id, A.name, U.firstname, U.lastname ';
-        $sql .= 'FROM cssr_course C ';
-        $sql .= 'LEFT JOIN cssr_area A ON A.id = C.area_id ';
-        $sql .= 'LEFT JOIN cssr_user U ON U.id = C.user_id ';
-        $sql .= 'WHERE C.user_id = :userId AND C.active = :active AND C.area_id IN ('.implode(',',array_values($removed)).') ';
-        $stmt = $em->getConnection()->prepare($sql);
-        $stmt->bindValue('userId', $staff->getId());
-        $stmt->bindValue('active', 1, \PDO::PARAM_INT);
-        $stmt->execute();
-        $toRemove = $stmt->fetchAll();
-        self::cancelCourses($em,$staff,$toRemove);
+        if ( !empty($removed) ) {
+            $sql  = 'SELECT C.id, C.area_id, C.user_id, A.name, U.firstname, U.lastname ';
+            $sql .= 'FROM cssr_course C ';
+            $sql .= 'LEFT JOIN cssr_area A ON A.id = C.area_id ';
+            $sql .= 'LEFT JOIN cssr_user U ON U.id = C.user_id ';
+            $sql .= 'WHERE C.user_id = :userId AND C.active = :active AND C.area_id IN ('.implode(',',array_values($removed)).') ';
+            $stmt = $em->getConnection()->prepare($sql);
+            $stmt->bindValue('userId', $staff->getId());
+            $stmt->bindValue('active', 1, \PDO::PARAM_INT);
+            $stmt->execute();
+            $toRemove = $stmt->fetchAll();
+            self::cancelCourses($em,$staff,$toRemove);
+        }
 
         // what to add
         $added = array_diff($courses, $currentCourses);
-        foreach ( $added as $areaId ) {
-
-            // check for previous course
-            $sql = 'SELECT 1 FROM cssr_course WHERE user_id = :staffId AND area_id = :areaId';
-            $stmt = $em->getConnection()->prepare($sql);
-            $stmt->bindValue('staffId', $staff->getId(), \PDO::PARAM_INT);
-            $stmt->bindValue('areaId', $areaId, \PDO::PARAM_INT);
-            $stmt->execute();
-            $hasPrevious = $stmt->fetch();
-
-            if ( $hasPrevious ) {
-                $sql  = 'UPDATE cssr_course ';
-                $sql .= 'SET active = :active, updated = :updated ';
-                $sql .= 'WHERE user_id = :staffId AND area_id = :areaId ';
+        if ( !empty($added) ) {
+            foreach ( $added as $areaId ) {
+                // check for previous course
+                $sql = 'SELECT 1 FROM cssr_course WHERE user_id = :staffId AND area_id = :areaId';
                 $stmt = $em->getConnection()->prepare($sql);
-                $stmt->bindValue('active', 1, \PDO::PARAM_INT);
-                $stmt->bindValue('updated', new \DateTime(), 'datetime');
                 $stmt->bindValue('staffId', $staff->getId(), \PDO::PARAM_INT);
                 $stmt->bindValue('areaId', $areaId, \PDO::PARAM_INT);
                 $stmt->execute();
-            } else {
-                $sql  = 'INSERT INTO cssr_course ( user_id, area_id, active, updated, created ) ';
-                $sql .= 'VALUES ( :staffId, :areaId, :active, :updated, :created ) ';
-                $stmt = $em->getConnection()->prepare($sql);
-                $stmt->bindValue('active', 1, \PDO::PARAM_INT);
-                $stmt->bindValue('updated', new \DateTime(), 'datetime');
-                $stmt->bindValue('created', new \DateTime(), 'datetime');
-                $stmt->bindValue('staffId', $staff->getId(), \PDO::PARAM_INT);
-                $stmt->bindValue('areaId', $areaId, \PDO::PARAM_INT);
-                $stmt->execute();
+                $hasPrevious = $stmt->fetch();
+
+                if ( $hasPrevious ) {
+                    $sql  = 'UPDATE cssr_course ';
+                    $sql .= 'SET active = :active, updated = :updated ';
+                    $sql .= 'WHERE user_id = :staffId AND area_id = :areaId ';
+                    $stmt = $em->getConnection()->prepare($sql);
+                    $stmt->bindValue('active', 1, \PDO::PARAM_INT);
+                    $stmt->bindValue('updated', new \DateTime(), 'datetime');
+                    $stmt->bindValue('staffId', $staff->getId(), \PDO::PARAM_INT);
+                    $stmt->bindValue('areaId', $areaId, \PDO::PARAM_INT);
+                    $stmt->execute();
+                } else {
+                    $sql  = 'INSERT INTO cssr_course ( user_id, area_id, active, updated, created ) ';
+                    $sql .= 'VALUES ( :staffId, :areaId, :active, :updated, :created ) ';
+                    $stmt = $em->getConnection()->prepare($sql);
+                    $stmt->bindValue('active', 1, \PDO::PARAM_INT);
+                    $stmt->bindValue('updated', new \DateTime(), 'datetime');
+                    $stmt->bindValue('created', new \DateTime(), 'datetime');
+                    $stmt->bindValue('staffId', $staff->getId(), \PDO::PARAM_INT);
+                    $stmt->bindValue('areaId', $areaId, \PDO::PARAM_INT);
+                    $stmt->execute();
+                }
             }
         }
     }
